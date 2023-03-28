35065c167b32316b090007e2c5f9aef621fd0d41
Ellie Barrera <elliediaz1204@gmail.com>
2023-03-28 15:28:22 -0400

    finished gallery and about me sections

--- src/Components/Helpers/Carousel.js
+++ src/Components/Helpers/Carousel.js
@@ -1,19 +1,34 @@
 import React, { useState } from 'react'
 import { CarouselData } from './CarouselData'
-import { faArrowAltCircleRight, faArrowAltCircleLeft } from 'react-icons/fa'
+import { VscChevronRight, VscChevronLeft  } from "react-icons/vsc";
 
 function Carousel({ slides }) {
 
     const [currImg, setCurrImg] = useState(0) 
     const length = slides.length
 
+    const nextSlide = () => {
+        setCurrImg(currImg === length-1 ? 0 : currImg + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrImg(currImg === 0 ? length-1  : currImg - 1)
+    }
+
+    if(!Array.isArray(slides) || slides.length <= 0) {
+        return null
+    }
+
     return (
         <div className='carousel'>
-            <faArrowAltCircleLeft className="left-arrow" />
-            <faArrowAltCircleRight className="right-arrow" />
+            <VscChevronLeft className="left-arrow" onClick={prevSlide}/>
+            <VscChevronRight className="right-arrow" onClick={nextSlide}/>
             {CarouselData.map((slide, index) => {
                 return (
-                    <img src={slide.img} alt="Pizzia Images" />
+                    <div className={index === currImg ? 'slide active': 'slide'} key={index}>
+                        {index === currImg && (<img src={slide.img} alt="Pizzia Images" className='image'/>)}
+                        
+                    </div>
                 )
             })}
         </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-import '../Styling/Components.css'
+import '../Styling/Components.css';
 import Carousel from "./Helpers/Carousel";
-import customers from "../Components/Assets/1.png";
-import pizza from "../Components/Assets/meatloverfull.jpg";
-import pizzatwo from "../Components/Assets/meatloverfull.jpg";
-import pizzathree from "../Components/Assets/meatloverfull.jpg";
-import Button from 'react-bootstrap/Button'
+import customers from "./Assets/1.png";
+import pizza from "./Assets/meatloverfull.jpg";
+import aboutusimg from "./Assets/aboutusimg.jpg";
+import Button from 'react-bootstrap/Button';
 import { CarouselData } from "./Helpers/CarouselData";
 
 
@@ -16,35 +15,50 @@ function Home() {
             <div className="landing-page">
                 <img className="customers" src={customers} alt="Customers"/>
                 <img className="pizza" src={pizza} alt="Pizza"/>
-                <div class="name">Pop's Pizza</div>
-                <div class="menu-link">
-                    <div class="favorites">Find Your Favorites Here</div>
+                <div className="name">Pop's Pizza</div>
+                <div className="menu-link">
+                    <div className="favorites">Find Your Favorites Here</div>
                     <div className="d-grid gap-2">
                         <Button variant="outline-light" size="lg" >ORDER NOW</Button>
                     </div>
                 </div>
             </div>
+
             <div className="about-page">
                 <div className="about-info">
-
+                    <h1 className="about-title">OUR STORY</h1>
+                    <p>Pop's Pizza is an establishment that bears the name of its founder, my grandfather affectionately referred to as Pop, who had always possessed an affinity for crafting pizzas. Pop had cultivated his pizza-making skills from an early age, working in various pizzerias and nurturing his dream of owning his own pizza parlor one day.</p>
+                    <p>As Pop matured, he accumulated sufficient resources to bring his long-held aspirations to fruition. He came across a petite shop in a bustling locality and launched Pop's Pizza.</p>
+                    <p>Initially, the business was sluggish, but Pop was undaunted. He labored strenuously in the kitchen, refining his recipes and concocting innovative toppings. His persistence paid off, and in no time, Pop's Pizza amassed a faithful following of patrons who were hooked on Pop's delectable pies.</p>
+                    <p>To this day, Pop's Pizza is a family-owned and operated venture, with Pop's grandchildren working alongside their parents to maintain the legacy that their grandfather started all those years ago.</p>
+                    <p>What is the secret behind Pop's Pizza's enduring prosperity? A lot of affection, commitment, and, of course, the most mouthwatering pizza around.</p>
                 </div>
-                <div className="about-image">
-
+                <div >
+                    <img className="about-image" src={aboutusimg} alt="Store Front" ></img>
                 </div>
 
             </div>
+
             <div className="gallery-page">
                 <div className="gallery-intro">
-                    <h1>Our Gallery</h1>
+                    <h1 className="gallery-title">Our Gallery</h1>
                 </div>
-                {/* <img className="gallery" src={pizzathree} alt="Pizza"/> */}
                 < Carousel slides={CarouselData} />
             </div>
+
             <div className="findus-page" id="findus">
                 <div className="findus-info">
+                    <h1>Location</h1>
+                    <p>1021 Portion Rd Lake Ronkonkoma, NY 11779</p>
+                    <h1>Hours</h1>
+                    <p>Daily: 11:30am - 8:00pm</p>
+                    <h1>Contact Us</h1>
+                    <p>Tel: (631) 736-3957</p>
+                    <p>Email: orderfrompops@gmail.com</p>
 
                 </div>
                 <div className="findus-location">
+                    
 
                 </div>
 
