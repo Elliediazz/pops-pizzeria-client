2aff8edca71c9452459a359b2af8e690a605d83d
Ellie Barrera <elliediaz1204@gmail.com>
2023-04-04 13:04:05 -0400

    finished landing page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -31,46 +31,57 @@ function Home() {
             <div className="about-page">
                 <div className="about-info">
                     <h1 className="about-title">OUR STORY</h1>
-                    <p>Pop's Pizza is an establishment that bears the name of its founder, my grandfather affectionately referred to as Pop, who had always possessed an affinity for crafting pizzas. Pop had cultivated his pizza-making skills from an early age, working in various pizzerias and nurturing his dream of owning his own pizza parlor one day.</p>
-                    <p>As Pop matured, he accumulated sufficient resources to bring his long-held aspirations to fruition. He came across a petite shop in a bustling locality and launched Pop's Pizza.</p>
-                    <p>Initially, the business was sluggish, but Pop was undaunted. He labored strenuously in the kitchen, refining his recipes and concocting innovative toppings. His persistence paid off, and in no time, Pop's Pizza amassed a faithful following of patrons who were hooked on Pop's delectable pies.</p>
-                    <p>To this day, Pop's Pizza is a family-owned and operated venture, with Pop's grandchildren working alongside their parents to maintain the legacy that their grandfather started all those years ago.</p>
-                    <p>What is the secret behind Pop's Pizza's enduring prosperity? A lot of affection, commitment, and, of course, the most mouthwatering pizza around.</p>
+                    <p>
+                        Pop's Pizza was named after its founder, my grandfather whom we called Pop, who had always had a love for making pizzas. Pop had been working in pizzerias since he was a teenager and had always dreamed of owning his own place one day.</p>
+                    <p>As he grew older, Pop saved up enough money to finally make his dream a reality. He found a small storefront in a bustling neighborhood and opened Pop's Pizza. At first, business was slow, but Pop didn't let that deter him. He spent long hours in the kitchen, perfecting his recipes and experimenting with new and innovative toppings. His hard work paid off, and before long, Pops Pizza had a loyal following of customers who couldn't get enough of Pop's delicious pies.
+                    </p>
+                    <p></p>
+                    <p>To this day, Pop's Pizza remains a family-owned and operated business, with Pop's grandchildren now working alongside their parents to carry on the legacy that their grandfather started all those years ago.</p>
+                    <p>The secret to Pop's Pizza's success? A whole lot of love, dedication, and of course, the best pizza around.</p>
                 </div>
                 <div >
                     <img className="about-image" src={aboutusimg} alt="Store Front" ></img>
                 </div>
 
             </div>
-
+            {/* <div className="reviews">
+                reviews
+            </div> */}
             <div className="gallery-page">
-                <div className="gallery-intro">
-                    <h1 className="gallery-title">Our Gallery</h1>
-                </div>
                 < Carousel slides={CarouselData} />
             </div>
 
             <div className="findus-page" id="findus">
                 <div className="findus-info">
-                    <h1>Location</h1>
-                    <p>1021 Portion Rd Lake Ronkonkoma, NY 11779</p>
-                    <h1>Hours</h1>
-                    <p>Daily: 11:30am - 8:00pm</p>
-                    <h1>Contact Us</h1>
-                    <p>Tel: (631) 736-3957</p>
-                    <p>Email: orderfrompops@gmail.com</p>
-
+                    <div className="info">
+                        <h3><BsFillGeoAltFill/>1021 Portion Rd Lake Ronkonkoma, NY 11779</h3>
+                    </div>
+                    <div className="info">
+                        <h3>Hours:</h3>
+                        <h6>Monday: 12:00am to 9:00 PM</h6>
+                        <h6>Tuesday: 11:30am to 9:00 PM</h6>
+                        <h6>Wednesday: 11:30am to 9:00 PM</h6>
+                        <h6>Thursday: 11:30am to 1:00 AM</h6>
+                        <h6>Friday: 11:30am to 1:00 AM</h6>
+                        <h6>Saturday: 11:30am to 1:00 PM</h6>
+                        <h6>Sunday: 12:00am to 8:00 PM</h6>
+                    </div>
+                    <div className="info">
+                        <h3>Contact Us:</h3>
+                        <h6>Tel: (631) 336-2762</h6>
+                        <h6>Email: <a href="mailto:orderfrompops@gmail.com">orderfrompops@gmail.com</a></h6>
+                    </div>
                 </div>
                 <div className="findus-location">
                 <iframe 
-                    title="Pop's Pizzeria Location"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3018.653783485661!2d-73.11125682445069!3d40.83556773004233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e839fd7cd0c873%3A0x6b9f4473ca4679e4!2spoppops%20pizzeria!5e0!3m2!1sen!2sus!4v1680619530005!5m2!1sen!2sus"
-                    width="100%" 
-                    height="100%" 
-                    style={{border:0}}
-                    allowfullscreen="" 
-                    loading="lazy" 
-                    referrerpolicy="no-referrer-when-downgrade">
+                title="Pop's Pizzeria Location"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3018.6358457042556!2d-73.07031882373137!3d40.83596167137524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e8472a9b5eaf69%3A0xbb4234f8cd17c135!2sPop&#39;s%20Pizza!5e0!3m2!1sen!2sus!4v1680625659673!5m2!1sen!2sus" 
+                width="100%" 
+                height="100%" 
+                style={{border:0}}
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
                 </iframe>
                 </div>
             </div>
